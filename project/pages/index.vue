fc00877b84404aa10751bfd2b9b4722ed4f39e30
Joshua Kim <Joshuak82@nycstudents.net>
2022-03-17 10:05:41 -0400

    tuj

--- project/pages/index.vue
+++ project/pages/index.vue
@@ -8,7 +8,11 @@
     </div>
 
     <h1>Satina's Food Blog</h1>
-    <h2></h2>
+    <h2>Menu</h2>
+    <div>
+      <il>Don't Be Scared To Explore Your Options</il>
+      <il>Don't Be Scared</il>
+    </div>
   </div>
 </template>
 
