0682a5c54aa045b04d465f7b03f19429fc53aa3d
Joshua Kim <Joshuak82@nycstudents.net>
2022-03-29 10:13:41 -0400

    sdfg

--- project/pages/index.vue
+++ project/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <h1>Satina's Food Blog</h1>
+    <div>
+      v-for= "blogpost in blogposts" :key = "blogpost.title"
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-}
+  data(){
+    return {
+      title: `Blog`
+    }
+  }
+async fetch() {
+  this.blogposts = await this.$content('blogposts').fetch()
+},}
+
 </script>
