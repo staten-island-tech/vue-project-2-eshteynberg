e5fa63c199fa79cb97c8ac29a324c57d08efa00f
eshteynberg <90703385+eshteynberg@users.noreply.github.com>
2022-03-31 10:13:01 -0400

    c

--- project/pages/index.vue
+++ project/pages/index.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
     <h1>Satina's Food Blog</h1>
-    <div>
-      v-for= "blogpost in blogposts" :key = "blogpost.title"
-    </div>
+    <div>v-for= "blogpost in blogposts" :key = "blogpost.title"</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  data(){
+  data() {
     return {
-      title: `Blog`
+      title: `Blog`,
     }
-  }
-async fetch() {
-  this.blogposts = await this.$content('blogposts').fetch()
-},}
-
+  },
+  async fetch() {
+    this.blogposts = await this.$content('blogposts').fetch()
+  },
+}
 </script>
