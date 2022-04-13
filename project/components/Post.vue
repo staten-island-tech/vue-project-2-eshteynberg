2432080289a4ea808e6ad7d15921c422b141cb8b
eshteynberg <90703385+eshteynberg@users.noreply.github.com>
2022-04-13 18:20:53 -0400

    ytrytr

--- project/components/Post.vue
+++ project/components/Post.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="Post">
-        <h3>{{ title }}</h3>
-        <h4>{{ desc }}</h4>
-    </div>
+  <div class="Post">
+    <h3>{{ title }}</h3>
+    <h4>{{ desc }}</h4>
+  </div>
 </template>
 
 <script>
 export default {
-props: {
+  props: {
     title: {
-        type: String,
-        default: 'Title'
+      type: String,
+      default: 'Title',
     },
     description: {
-        type: String, 
-        default: 'Description'
+      type: String,
+      default: 'Description',
     },
-}
+  },
 }
 </script>
 
 <style>
-.Post { 
-    width: 40%;
-    display: flex;
+.Post {
+  width: 40%;
+  display: flex;
+  border: solid black;
 }
 </style>
--- project/pages/index.vue
+++ project/pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <body>
-    
-    <splash class="test"></splash>
-    <!-- <div><Post
-    v-for= "blogpost in blogposts" :key = "blogpost.title" :title="blogpost.title" :desc="blogpost.description"></Post></div> -->
-     </body>
+      <splash class="test"></splash>
+      <div>
+        <Post
+          v-for="blogpost in blogposts"
+          :key="blogpost.title"
+          :title="blogpost.title"
+          :desc="blogpost.description"
+        ></Post>
+      </div>
+    </body>
   </div>
-  
 </template>
 
 
@@ -16,30 +20,26 @@ export default {
   name: 'IndexPage',
   data() {
     return {
-      blogposts:[],
+      blogposts: [],
     }
   },
-  head(){
-    return{
-      title:`Blog`
+  head() {
+    return {
+      title: `Blog`,
     }
   },
   async fetch() {
     this.blogposts = await this.$content('blogposts').fetch()
   },
-
-
 }
 </script>
 
 <style>
-
-body{
-  background-color: #B2966F;
+body {
+  background-color: #b2966f;
 }
 
-.test{
+.test {
   object-fit: contain;
 }
-
 </style>
