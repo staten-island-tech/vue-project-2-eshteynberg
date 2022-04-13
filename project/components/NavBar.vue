1cbc1ce7cd779cf34d5aac5550241d985814a821
eshteynberg <90703385+eshteynberg@users.noreply.github.com>
2022-04-13 18:17:41 -0400

    gfgh

--- project/components/NavBar.vue
+++ project/components/NavBar.vue
@@ -1,41 +1,40 @@
 <template>
-    <div class="NavBar">
-        <img class="coffee" src="./img/coffee.png" alt="">
-        <a class="btn" href='\pages\index.vue'>Home</a>
-        <a class="btn" href='/about'>About</a>
-        <a class="btn" href='/cuisines'>Cuisines</a>
-        <a class="btn">Contact Us</a>
-        
-    </div>
+  <div class="NavBar">
+    <img class="coffee" src="./img/coffee.png" alt="" />
+    <a class="btn" href="/index">Home</a>
+    <a class="btn" href="/about">About</a>
+    <a class="btn" href="/cuisines">Cuisines</a>
+    <a class="btn">Contact Us</a>
+  </div>
 </template>
 
 <style scoped>
-.NavBar{
-    background-color: #B2966F;
-    height: 6rem;
+.NavBar {
+  background-color: #b2966f;
+  height: 6rem;
 }
 
-.coffee{ 
-    z-index: -1;
-    margin-left: 3rem;
+.coffee {
+  z-index: -1;
+  margin-left: 3rem;
 }
 
-.btn    {
-    display: inline-block;
-    background-color: #B2966F;
-    color: white;
-    flex-flow: row;
-    justify-content: space-between;
-    margin-bottom: 1rem;
-    margin-left: 18rem;
-    border: #B2966F solid ;
-    font-size: 1.25rem;
-    text-align: center;
+.btn {
+  display: inline-block;
+  background-color: #b2966f;
+  color: white;
+  flex-flow: row;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  margin-left: 18rem;
+  border: #b2966f solid;
+  font-size: 1.25rem;
+  text-align: center;
 }
 </style>
 
 <script>
 export default {
-    navbar: Object,
+  navbar: Object,
 }
 </script>
