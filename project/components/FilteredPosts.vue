5847932cb5e0452378b4ccd941b56473cd02e74d
eshteynberg <90703385+eshteynberg@users.noreply.github.com>
2022-05-28 19:21:07 -0400

    com

--- project/components/FilteredPosts.vue
+++ project/components/FilteredPosts.vue
@@ -4,43 +4,40 @@
       <!-- <div class="title">
       <h1>Recent Posts</h1>
 </div> -->
- 
-<section class="ameri">
+      <h1>Featured Posts</h1>
+      <section class="ameri">
         <div class="child1">
-        <img class="img" src="../static/img/ameri1.jpg" alt="" />
-       
+          <img class="img" src="../static/img/ameri1.jpg" alt="" />
         </div>
         <div class="child2">
-        <img class="img" src="../static/img/ameri2.jpg" alt="" />
-</div>
-<div class="child3">
-        <img class="img" src="../static/img/ameri3.jpg" alt="" />
+          <img class="img" src="../static/img/ameri2.jpg" alt="" />
+        </div>
+        <div class="child3">
+          <img class="img" src="../static/img/ameri3.jpg" alt="" />
         </div>
         <div class="child4">
-        <img class="img" src="../static/img/ameri4.jpg" alt="" />
-</div>
-<div class="child5">
-        <img class="img" src="../static/img/ameri5.jpg" alt="" />
+          <img class="img" src="../static/img/ameri4.jpg" alt="" />
         </div>
+        <div class="child5">
+          <img class="img" src="../static/img/ameri5.jpg" alt="" />
+        </div>
+      </section>
 
-</section>
-
-<section class="mediterannean">
+      <section class="mediterannean">
         <div class="child6">
-        <img class="img" src="../static/img/med1.jpg" alt="" />
-</div>
-<div class="child7">
-        <img class="img" src="../static/img/med2.jpg" alt="" />
-</div>
-<div class="child8">
-        <img class="img" src="../static/img/med3.jpg" alt="" />
-</div>
-<div class="child9">
-        <img class="img" src="../static/img/med4.jpg" alt="" />
-</div>
-</section>
-      </div>
-
+          <img class="img" src="../static/img/med1.jpg" alt="" />
+        </div>
+        <div class="child7">
+          <img class="img" src="../static/img/med2.jpg" alt="" />
+        </div>
+        <div class="child8">
+          <img class="img" src="../static/img/med3.jpg" alt="" />
+        </div>
+        <div class="child9">
+          <img class="img" src="../static/img/med4.jpg" alt="" />
+        </div>
+      </section>
+    </div>
   </body>
 </template>
 
@@ -58,16 +55,16 @@ font-size: 1.5rem;
   background-color: #c09165;
   padding: 10rem;
   width: 48rem;
-margin: auto;
-margin-top: 5rem;
-display: grid;
-gap: 3rem;
-grid-template-columns: repeat(3,1fr);
-justify-content: center;
-align-content: space-between;
+  margin: auto;
+  margin-top: 5rem;
+  display: grid;
+  gap: 3rem;
+  grid-template-columns: repeat(3, 1fr);
+  justify-content: center;
+  align-content: space-between;
 }
 
-.child1{
+.child1 {
   grid-column: span 2;
 }
 .img {
@@ -75,10 +72,15 @@ align-content: space-between;
   height: 60%;
   padding: 2rem;
   box-shadow: 0px 0px 40px 20px rgba(0, 0, 0, 0.05);
-
 }
 
-
-
-
+.h1 {
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  align-content: center;
+  grid-template-columns: 1fr;
+  text-align: center;
+  font-family: raleway;
+  font-size: 3.5rem;
+}
 </style>
